cd2a9cc3fb1ed7a156a8dd9847d74eb3fe50c229
Butra Balaprasad <butrabalaprasad370@gmail.com>
2025-07-12 22:16:40 +0530

    Improve CI: run tests & explore

---
+++ tests/stringUtils.test.js
@@ -0,0 +1,29 @@
+const { capitalize, reverse } = require('../index');
+
+describe('String Utils', () => {
+  test('capitalize should capitalize each word', () => {
+    expect(capitalize('hello world')).toBe('Hello World');
+    expect(capitalize('this is test')).toBe('This Is Test');
+  });
+
+  test('reverse should reverse the string', () => {
+    expect(reverse('hello')).toBe('olleh');
+    expect(reverse('')).toBe('');
+  });
+});
+
+
+
+describe('String Utils 2', () => {
+    test('capitalize should capitalize each word', () => {
+      expect(capitalize('hello world')).toBe('Hello World');
+      expect(capitalize('this is test')).toBe('This Is Test');
+    });
+  
+    test('reverse should reverse the string', () => {
+      expect(reverse('hello')).toBe('olleh');
+      expect(reverse('')).toBe('');
+    });
+  });
+
+  
